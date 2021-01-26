c51fb0340a9a9bdf09f0f9c5befa68d93340ed18
Cláudio Medeiros <klawdyo@gmail.com>
2021-01-25 23:02:39 -0300

    Inclusão de opengraph

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json'
+import Header from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -28,6 +29,12 @@ const theme = db.theme;
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Header>
+        <meta property="og:image" content={db.bg} />
+        <meta property="og:url" content={db.external} />
+        <meta property="og:title" content={db.title} />
+        <meta property="og:description" content={db.description} />
+      </Header>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
