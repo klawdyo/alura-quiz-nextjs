b38e188d245f0132c4133299bc19070ab0cda4ab
Cláudio Medeiros <klawdyo@gmail.com>
2021-01-31 12:39:21 -0300

    api com cabeçalhos

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,6 +1,6 @@
 import db from '../../db.json';
 
-function Db(response, request) {
+function Db(request, response) {
   if (request.method === 'OPTIONS') {
     response.status(200).end();
     return;
@@ -9,6 +9,8 @@ function Db(response, request) {
   response.setHeader('Access-Control-Allow-Credentials', true);
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
+  response.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
 
   response.json(db);
 }
